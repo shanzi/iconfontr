f5901d9360ae5657a392c2fc2a262aa1de3a48df
Chase Zhang <yun.er.run@gmail.com>
2014-04-12 22:00:32 +0800

    Initial Commit

---
+++ iconfontr/IFDocument.h
@@ -0,0 +1,13 @@
+//
+//  IFDocument.h
+//  iconfontr
+//
+//  Created by Chase Zhang on 4/12/14.
+//  Copyright (c) 2014 io-meter. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface IFDocument : NSDocument
+
+@end
---
+++ iconfontr/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  iconfontr
+//
+//  Created by Chase Zhang on 4/12/14.
+//  Copyright (c) 2014 io-meter. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+  return NSApplicationMain(argc, argv);
+}
