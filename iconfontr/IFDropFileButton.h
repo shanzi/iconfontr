5759ee10ac7599c1444984053796d8861fdc2487
Chase Zhang <yun.er.run@gmail.com>
2014-05-19 21:02:51 +0800

    add icon font importer and drag drop

---
+++ iconfontr/IFDropFileButton.h
@@ -0,0 +1,15 @@
+//
+//  IFDropFileButton.h
+//  iconfontr
+//
+//  Created by Chase Zhang on 5/19/14.
+//  Copyright (c) 2014 io-meter. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface IFDropFileButton : NSButton
+
+@property(nonatomic, readonly)NSURL *fileURL;
+
+@end
---
+++ iconfontr/IFIconImporterWindowController.h
@@ -0,0 +1,15 @@
+//
+//  IFIconImportWindowController.h
+//  iconfontr
+//
+//  Created by Chase Zhang on 5/19/14.
+//  Copyright (c) 2014 io-meter. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface IFIconImporterWindowController : NSWindowController
+
+- (IBAction)droppedFile:(id)sender;
+
+@end
